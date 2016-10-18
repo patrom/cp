5f8e161b3f74c1e07a5b29cd96b39e69b904670e
prombouts <patrik.rombouts@telenet.be>
2016-10-18 18:59:42 +0200

    four voice composition

---
+++ cp/src/main/java/cp/composition/FourVoiceComposition.java
@@ -0,0 +1,11 @@
+package cp.composition;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by prombouts on 18/10/2016.
+ */
+
+@Component(value="fourVoiceComposition")
+public class FourVoiceComposition extends  Composition {
+}
